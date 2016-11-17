3a70e666bf06d0d04e0c83862db440625e1e897c
ElRobinson <luisfelipe.robinson@gmail.com>
2016-11-16 23:52:04 -0200

    Image Picker

---
+++ app/src/main/java/com/robinson/luis/chat/ImagePicker.java
@@ -0,0 +1,10 @@
+package com.robinson.luis.chat;
+
+/**
+ * Created by Luis on 16/11/2016.
+ */
+
+public class ImagePicker {
+
+    private static final int DEFAULT_MIN_WIDTH_QUALITY = 400; //min pixels
+}
